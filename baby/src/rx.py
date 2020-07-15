597c144e3ad3e6f3ba39d8f3afed0091b1c3ea61
Baby <nlemus@umich.edu>
2020-07-14 23:39:50 -0400

    minor updates

--- baby/src/rx.py
+++ baby/src/rx.py
@@ -1,39 +1,45 @@
 #!/usr/bin/python3
 import time
 import serial
+import rospy
 
-print("UART Demonstration Program")
-print("NVIDIA Jetson Nano Developer Kit")
+print("reading")
+
+def readEncoders():
+	try:
+		while True:	
+			if serial_port.inWaiting() > 0:
+				encoderticks = serial_port.read(3)
+				print(encoderticks)
+				#penis = int.from_bytes(encoderticks, byteorder='little')
+		
+				#print(encoderticks)
+			
+	except KeyboardInterrupt:
+		print("Exiting Program")
+
+	except Exception as exception_error:
+		print("Error occurred. Exiting Program")
+		print("Error: " + str(exception_error))
+
+	finally:
+		serial_port.close()
+	pass
+
+if __name__ == '__main__':
+	serial_port = serial.Serial(
+	port="/dev/ttyTHS1",
+	baudrate=19200,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        )
+
+	rospy.init_node('rx', anonymous=True)	
+	rate = rospy.Rate(30)
+	readEncoders()
+	rospy.spin()
 
 
-serial_port = serial.Serial(
-    port="/dev/ttyTHS1",
-    baudrate=19200,
-    bytesize=serial.EIGHTBITS,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-)
-# Wait a second to let the port initialize
-time.sleep(1)
 
-try:
-    # Send a simple header
-    serial_port.write("UART Demonstration Program\r\n".encode())
-    serial_port.write("NVIDIA Jetson Nano Developer Kit\r\n".encode())
-    while True:
-        if serial_port.inWaiting() > 0:
-            data = serial_port.read()
-            print(data)
-
-
-except KeyboardInterrupt:
-    print("Exiting Program")
-
-except Exception as exception_error:
-    print("Error occurred. Exiting Program")
-    print("Error: " + str(exception_error))
-
-finally:
-    serial_port.close()
-    pass
 
